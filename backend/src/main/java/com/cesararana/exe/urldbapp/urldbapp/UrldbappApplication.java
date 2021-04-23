c27e91905e2e5ba25da5a8c6b77b3a1dc6a149a5
Cesar Augusto Arana Collazos <me@cesararana.com>
2021-04-23 15:50:57 -0500

    Initial

---
+++ backend/src/main/java/com/cesararana/exe/urldbapp/urldbapp/UrldbappApplication.java
@@ -0,0 +1,13 @@
+package com.cesararana.exe.urldbapp.urldbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrldbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrldbappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cesararana/exe/urldbapp/urldbapp/UrldbappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesararana.exe.urldbapp.urldbapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrldbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
